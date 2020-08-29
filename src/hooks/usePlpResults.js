fb2c1ade5ea31f72e2acbf86e78ddce1881e41bf
Erik Ralston <erikralston310@gmail.com>
2020-08-28 18:10:25 -0700

    issue #2: WIP - trying to figure out how to get filtered products to load

--- src/hooks/usePlpResults.js
+++ src/hooks/usePlpResults.js
@@ -5,29 +5,85 @@ const usePlpResults = (selectedRefinements, sort, itemsPerPage, activePage, requ
     // NOTE: only passing requestPath for now to differentiate between the static results
 
     const [plpResults, setPlpResults] = useState({});
-    const baseURL = 'http://localhost:3000/'
+    const baseURL = 'https://5f4983e18e271c001650ca8f.mockapi.io';
+    const selectedRefinementsArr = selectedRefinements.split('+');
+    let query = '';
+    let plpData = {};
+    let facetsData = [];
+    let productsData = [];
+
+    const onDataLoaded = () => {
+        //console.log('selectedRefinementsArr', selectedRefinementsArr);
+            //console.log(plpData);
+            //console.log(facetsData);
+            //console.log(productsData);
+
+            let availableProducts = productsData.map((product) => {
+                let facetIds = product.facetIds;
+                let availableProduct = null;
+                
+                facetIds.forEach((facetId) => {
+                    if (selectedRefinementsArr.includes(facetId)) {
+                        availableProduct = product;
+                    }
+                });
+                return availableProduct;
+            });
+
+            plpData.products = availableProducts;
+            plpData.facets = facetsData;
+
+            console.log(plpData);
+
+            setPlpResults(plpData);
+
+            window.history.pushState({}, '', `?${query}`);
+            const navEvent = new PopStateEvent('popstate');
+            window.dispatchEvent(navEvent);
+    };
 
     useEffect(() => {
-        plpRequest(selectedRefinements, sort, itemsPerPage, activePage, requestPath);
+        plpRequest(selectedRefinements, sort, itemsPerPage, activePage, requestPath)
+            .then(() => {
+                onDataLoaded();
+            });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [selectedRefinements, sort, itemsPerPage, activePage, requestPath]);
 
     const plpRequest = async (facetsParam, sortParam, itemsPerPageParam, pageStartIndexParam, newRequestPath) => {
         try {
-            let url = new URL(`${baseURL}${newRequestPath}`);
-            // NOTE: building the query string this way because using toString() on a params object causes the "+" that
-            // concatenates facets values to be encoded. Is there a better workaround?
-            let query = `facets=${facetsParam}&sort=${sortParam}&itemsPerPage=${itemsPerPageParam}&pageStartIndex=${pageStartIndexParam}`;
-            let response = await fetch (`${url}?${query}`, { method: 'GET' });
+            query = `facets=${facetsParam}&sort=${sortParam}&itemsPerPage=${itemsPerPageParam}&pageStartIndex=${pageStartIndexParam}`;
 
-            if (!response.ok) {
-                throw new Error(`There was an error loading the requested data. status: ${response.status}`);
+            let plpUrl = new URL(`${baseURL}/plp`);
+            let productsUrl = new URL(`${baseURL}/products`);
+            let facetsUrl = new URL(`${baseURL}/facets`);
+            
+            // PLP request
+            let plpResponse = await fetch (`${plpUrl}`, { method: 'GET' });
+
+            if (!plpResponse.ok) {
+                throw new Error(`There was an error loading the PLP data. status: ${plpResponse.status}`);
             } else {
-                let data = await response.json();
-                setPlpResults(data);
-                window.history.pushState({}, '', `?${query}`);
-                const navEvent = new PopStateEvent('popstate');
-                window.dispatchEvent(navEvent);
+                let plpDataArr = await plpResponse.json();
+                plpData = plpDataArr[0];
+            }
+
+            // Facets request
+            let facetsResponse = await fetch (`${facetsUrl}`, { method: 'GET' });
+
+            if (!facetsResponse.ok) {
+                throw new Error(`There was an error loading the products data. status: ${facetsResponse.status}`);
+            } else {
+                facetsData = await facetsResponse.json();
+            }
+
+            // Products request
+            let productsResponse = await fetch (`${productsUrl}`, { method: 'GET' });
+
+            if (!productsResponse.ok) {
+                throw new Error(`There was an error loading the products data. status: ${productsResponse.status}`);
+            } else {
+                productsData = await productsResponse.json();
             }
         } catch (error) {
             alert(`There was an error loading the requested data. status: ${error}`)
