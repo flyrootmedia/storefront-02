c2a42888d47c9397443e0832dc2e547d1b2d4167
Erik Ralston <erikralston310@gmail.com>
2020-08-29 19:11:59 -0700

    issue #2: WIP - added keys to api to allow for sorting and filtering; added logic to filter and sort results, to set total item count in UI, and to set selected sort option

--- src/hooks/usePlpResults.js
+++ src/hooks/usePlpResults.js
@@ -1,94 +1,134 @@
 import { useState, useEffect } from 'react';
 
-const usePlpResults = (selectedRefinements, sort, itemsPerPage, activePage, requestPath) => {
-
-    // NOTE: only passing requestPath for now to differentiate between the static results
-
+const usePlpResults = (selectedRefinements, sort, itemsPerPage, activePage) => {
     const [plpResults, setPlpResults] = useState({});
     const baseURL = 'https://5f4983e18e271c001650ca8f.mockapi.io';
-    const selectedRefinementsArr = selectedRefinements.split('+');
+    let selectedRefinementsArr = [];
     let query = '';
-    let plpData = {};
-    let facetsData = [];
-    let productsData = [];
 
-    const onDataLoaded = () => {
-        //console.log('selectedRefinementsArr', selectedRefinementsArr);
-            //console.log(plpData);
-            //console.log(facetsData);
-            //console.log(productsData);
+    const onDataLoaded = (plpData, newSort) => {
+        let facetsData = plpData.facets;
+        let productsData = plpData.products;
+        let availableProducts = [];
+        let isFiltered = false;
 
-            let availableProducts = productsData.map((product) => {
+        // if more than just the base category facet is selected, 
+        // determine if the products match the selected refinements and update the product array
+        if (selectedRefinementsArr.length > 1) {
+            isFiltered = true;
+
+            productsData.forEach((product) => {
                 let facetIds = product.facetIds;
-                let availableProduct = null;
-                
-                facetIds.forEach((facetId) => {
-                    if (selectedRefinementsArr.includes(facetId)) {
-                        availableProduct = product;
-                    }
+                let refinementIsAvailable = [];
+            
+                selectedRefinementsArr.forEach((refinementId) => {
+                    refinementIsAvailable.push(facetIds.includes(refinementId));
                 });
-                return availableProduct;
+                
+                if (refinementIsAvailable.indexOf(true) > -1) {
+                    availableProducts.push(product);
+                }
             });
+        }
 
-            plpData.products = availableProducts;
-            plpData.facets = facetsData;
+        const sortProducts = ((products) => {
 
-            console.log(plpData);
+            let sortedProducts = [];
 
-            setPlpResults(plpData);
+            switch (newSort) {
+                case 'brandName':
+                    sortedProducts = products.sort((a, b) => (a.name > b.name) ? 1 : -1);
+                    break;
+                case 'topRated':
+                    sortedProducts = products.sort((a, b) => (a.ratings.rating < b.ratings.rating) ? 1 : -1);
+                    break;
+                case 'priceLowToHigh':
+                    sortedProducts = products.sort((a, b) =>  (a.price.lowPrice > b.price.lowPrice) ? 1 : -1);
+                    break;
+                case 'priceHighToLow':
+                    sortedProducts = products.sort((a, b) =>  (a.price.highPrice < b.price.highPrice) ? 1 : -1);
+                    break;
+                case 'bestSellers':
+                    sortedProducts = products.sort((a, b) =>  (b.isBestSeller) ? 1 : -1);
+                    break;
+                case 'featured':
+                    sortedProducts = products.sort((a, b) =>  (b.isFeatured) ? 1 : -1);
+                    break;
+                case 'newestArrivals':
+                    sortedProducts = products.sort((a, b) =>  (b.isNewArrival) ? 1 : -1);
+                    break;
+                default:
+                    sortedProducts = products;
+                    break;
 
-            window.history.pushState({}, '', `?${query}`);
-            const navEvent = new PopStateEvent('popstate');
-            window.dispatchEvent(navEvent);
+            }
+
+            return sortedProducts;
+        });
+        
+        if (isFiltered) {
+            plpData.products = sortProducts(availableProducts);
+        } else {
+            plpData.products = sortProducts(plpData.products);
+        }
+
+        // set the number of results
+        plpData.itemCount = plpData.products.length;
+
+        // set the active sort
+        plpData.sortOptions.forEach((option) => {
+            option.selected = option.value === newSort;
+        });
+
+        // determine if the refinement is selected and set the selected state
+        facetsData.forEach((facet) => {
+            let refinementsArr = facet.refinements;
+
+            refinementsArr.forEach((refinement) => {
+                refinement.isSelected = selectedRefinementsArr.includes(refinement.id);
+            });
+        });
+
+        plpData.facets = facetsData;
+
+        // Sort
+
+
+        // Finally, set the whole PLP object
+        setPlpResults(plpData);
+
+        window.history.pushState({}, '', `?${query}`);
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
     };
 
     useEffect(() => {
-        plpRequest(selectedRefinements, sort, itemsPerPage, activePage, requestPath)
-            .then(() => {
-                onDataLoaded();
-            });
+        plpRequest(selectedRefinements, sort, itemsPerPage, activePage);
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [selectedRefinements, sort, itemsPerPage, activePage, requestPath]);
+    }, [selectedRefinements, sort, itemsPerPage, activePage]);
 
-    const plpRequest = async (facetsParam, sortParam, itemsPerPageParam, pageStartIndexParam, newRequestPath) => {
+    const plpRequest = async (facetsParam, sortParam, itemsPerPageParam, pageStartIndexParam) => {
         try {
-            query = `facets=${facetsParam}&sort=${sortParam}&itemsPerPage=${itemsPerPageParam}&pageStartIndex=${pageStartIndexParam}`;
+            const plpUrl = new URL(`${baseURL}/plp`);
+            selectedRefinementsArr = facetsParam.split('+');
+            query = new URLSearchParams({
+                facets: facetsParam,
+                sort: sortParam,
+                itemsPerPage: itemsPerPageParam,
+                pageStartIndex: pageStartIndexParam
+            });
 
-            let plpUrl = new URL(`${baseURL}/plp`);
-            let productsUrl = new URL(`${baseURL}/products`);
-            let facetsUrl = new URL(`${baseURL}/facets`);
-            
-            // PLP request
             let plpResponse = await fetch (`${plpUrl}`, { method: 'GET' });
 
             if (!plpResponse.ok) {
                 throw new Error(`There was an error loading the PLP data. status: ${plpResponse.status}`);
             } else {
                 let plpDataArr = await plpResponse.json();
-                plpData = plpDataArr[0];
-            }
-
-            // Facets request
-            let facetsResponse = await fetch (`${facetsUrl}`, { method: 'GET' });
-
-            if (!facetsResponse.ok) {
-                throw new Error(`There was an error loading the products data. status: ${facetsResponse.status}`);
-            } else {
-                facetsData = await facetsResponse.json();
-            }
-
-            // Products request
-            let productsResponse = await fetch (`${productsUrl}`, { method: 'GET' });
-
-            if (!productsResponse.ok) {
-                throw new Error(`There was an error loading the products data. status: ${productsResponse.status}`);
-            } else {
-                productsData = await productsResponse.json();
+                onDataLoaded(plpDataArr[0], sortParam);
             }
         } catch (error) {
             alert(`There was an error loading the requested data. status: ${error}`)
         }
-        
     };
 
     return [plpResults, plpRequest];
